9d406b5f81e2b05951b72da5b81860f8329d0b98
sandro <sandrokakashvili@gmail.com>
2019-09-14 16:22:20 +0400

    Updates

---
+++ precache-manifest.1d6c09d60b69f5266f18fb667526b45f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a8018434f71cee0f5e29f087415199",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f1075a91ace231f8311",
+    "url": "/monsters-rolodex/static/css/main.ee8eb7d3.chunk.css"
+  },
+  {
+    "revision": "72558d03f2bb77701f61",
+    "url": "/monsters-rolodex/static/js/2.6a5af712.chunk.js"
+  },
+  {
+    "revision": "6f1075a91ace231f8311",
+    "url": "/monsters-rolodex/static/js/main.28e31a53.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
